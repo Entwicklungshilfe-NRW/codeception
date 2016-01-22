33cca68d60e4d90045bdc20e872ec4deabac06ce
Roland Golla <rgolla@rogo.it>
2016-01-22 02:12:21 +0100

    backend login test failed

--- tests/acceptance/BackendLoginCept.php
+++ tests/acceptance/BackendLoginCept.php
@@ -5,3 +5,9 @@ $I->amOnPage('/typo3');
 $I->waitForElement('#t3-username');
 $I->seeInCurrentUrl('/typo3/');
 
+$I->wantToTest('Login failed');
+$I->fillField('#t3-username', 'testify');
+$I->fillField('#t3-password', 'jsdlabvj');
+$I->click('Login');
+$I->waitForElement('body');
+$I->seeNumberOfElements('#t3-login-error', 1);
