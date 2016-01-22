62041c06975871be59231564b1abd28e24f34bde
Roland Golla <rgolla@rogo.it>
2016-01-22 02:15:39 +0100

    fix autocomplete search is removed

--- tests/acceptance/SearchCept.php
+++ tests/acceptance/SearchCept.php
@@ -6,13 +6,3 @@ $I->fillField('#form-element-21', 'Drupal');
 $I->click('#solr-website-search button');
 $I->waitForElement('h2');
 $I->see('Migrating Drupal to TYPO3');
-
-$I->wantTo('check autocomplete');
-$I->amOnPage('/');
-$I->fillField('#form-element-21', 'rel');
-$I->waitForElement('.ui-autocomplete > li');
-$I->seeLink('relations');
-$I->Click('relations');
-$I->waitForElement('#tx-solr-q');
-$search = $I->grabValueFrom('#tx-solr-q');
-$this->assertEquals($search, 'relations');
